f715429923e474493ab21927d6b74e0f4a448970
Waeoo <davin50@hotmail.com>
2022-03-11 15:58:36 +0000

    Add move functions

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import os
 
+# count steps taken
+STEPS = 0
+
 
 def clear():
     os.system("cls" if os.name == "nt" else "clear")
@@ -8,6 +11,30 @@ def clear():
 def run_game():
     clear()
     print("Welcome to Hero Quest")
+
+
+def move_left():
+    clear()
+    steps_check()
+
+
+def move_right():
+    clear()
+    steps_check()
+
+
+def move_forward():
+    clear()
+    steps_check()
+
+
+def steps_check():
+    if not STEPS <= 10:
+        end_game()
+
+
+def end_game():
+    pass
 
 
 run_game()
