2bdd9d297ddbb5ec09c5bcc16d650065be349cfc
Waeoo <davin50@hotmail.com>
2022-03-11 15:49:06 +0000

    Add clear function

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
 def run_game():
+    clear()
     print("Welcome to Hero Quest")
+
 
 run_game()
